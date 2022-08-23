57beb5eb07190156c50bf56171d118a55a2dab1f
Dell <Dell@DESKTOP-K1HBLQ5>
2022-08-23 23:39:43 +0530

    scanner demo

---
+++ CoreJava/src/com/tns/corejava/FlowDemo.java
@@ -0,0 +1,19 @@
+package com.tns.corejava;
+
+public class FlowDemo
+{
+	public static void main(String[] args)
+	{
+		int x=10;
+		
+		if(x==100)                               //false
+		{
+			System.out.println("Good Morning");
+		}
+		else
+		{
+			System.out.println("Good Evening");
+		}
+	}
+
+}
